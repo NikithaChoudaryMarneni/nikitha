707e06f31a90eb349cd939a82d99fe1dcd33bb62
NikithaChoudaryMarneni <nikhita.choudarymarneni1@t-mobile.com>
2024-03-22 20:07:52 -0400

    changes for task3

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,18 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should be able to search books by title', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('.mat-focus-indicator.mat-flat-button.mat-button-base.mat-primary').first().click();
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
+  });
+
+  it('When: we have one element in reading list', () => {
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('[data-testing="remove-reading-list"]').click();
+    cy.get('.mat-button-base').contains('Undo').click();
+  });
+
 });
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -4,14 +4,16 @@ import {
   addToReadingList,
   clearSearch,
   getAllBooks,
+  getReadingList,
   ReadingListBook,
+  removeFromReadingList,
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
-import { Book } from '@tmo/shared/models';
+import { Book, ReadingListItem } from '@tmo/shared/models';
 import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-
+import { debounce, debounceTime } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
@@ -19,7 +21,8 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class BookSearchComponent implements OnInit {
   books: ReadingListBook[];
-
+  itemList: ReadingListItem[];
+  readingList = this.store.select(getReadingList);
   searchForm = this.fb.group({
     term: ''
   });
@@ -28,38 +31,53 @@ export class BookSearchComponent implements OnInit {
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private snackBar: MatSnackBar
   ) {
     this.bookSubject.pipe(
       debounceTime(500)
     ).subscribe((res) => this.searchBooks());
+    this.readingList.subscribe(
+      itemList => {
+        this.itemList = itemList;
+      }
+    )
   }
-
   get searchTerm(): string {
     return this.searchForm.value.term;
   }
-
   ngOnInit(): void {
     this.store.select(getAllBooks).subscribe(books => {
       this.books = books;
     });
   }
-
   formatDate(date: void | string) {
     return date
       ? new Intl.DateTimeFormat('en-US').format(new Date(date))
       : undefined;
   }
-
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
+    this.actionConfirmation(
+      'Adding To reading list ' + book.title,
+      this.removeFromReadingList,
+      this.itemList
+    )
   }
-
+  removeFromReadingList = (data:ReadingListItem[]) => {
+    const item = data[data.length-1];
+    this.store.dispatch(removeFromReadingList({ item }));
+  }
+  actionConfirmation(msg, func, data) {
+    const snackBarRef = this.snackBar.open(msg, 'Undo');
+    snackBarRef.onAction().subscribe(() => {
+      func(data);
+    });
+  }
   searchExample() {
     this.searchForm.controls.term.setValue('javascript');
     this.searchBooks();
   }
-
   searchBooks() {
     if (this.searchForm.value.term) {
       this.store.dispatch(searchBooks({ term: this.searchTerm }));
@@ -71,5 +89,4 @@ export class BookSearchComponent implements OnInit {
   searchBookTypeAhead() {
     this.bookSubject.next();
   }
-
 }
